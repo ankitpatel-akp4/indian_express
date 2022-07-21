a7360b592a92ea793aeb7a31abe6211e149a8e96
indicate0 <indicate0@gmail.com>
2022-07-21 07:04:36 +0530

    day-1 commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let cross = document.querySelector('#cross')
+cross.addEventListener('click',()=>{
+    let a = document.querySelector('#menuContent')
+    a.style.display = 'none'
+console.log(5);
+})
+let menu = document.querySelector('#menu')
+menu.addEventListener('click',()=>{
+    let a = document.querySelector('#menuContent')
+    a.style.display = 'block'
+console.log(5);
+})
+let search = document.getElementById('search')
+search.addEventListener('click',()=>{
+    document.getElementById('searchContent').style.display = 'block'
+    document.getElementById('hide').style.display = 'block'
+    console.log(7)
+})
+let x = document.getElementById('x')
+x.addEventListener('click',()=>{
+    document.getElementById('searchContent').style.display = 'none'
+    document.getElementById('hide').style.display = 'none'
+    console.log(8)
+})
